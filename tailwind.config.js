cd84529048e951853b36054fb6788dec5b47f0c9
t <tysonpederson95@gmail.com>
2023-02-02 21:23:06 -0600

    Set up navbar, hero, the space section, and footer

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "mytheme",
+    themes: [
+      {
+        mytheme: {
+          "primary": "#1E3A8A",
+          "secondary": "#CA8A04",
+          "accent": "#37CDBE",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+}
